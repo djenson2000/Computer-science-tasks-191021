87531b489872cee5a62faba08eeaabc9c971a534
Drew Hunt <21234303@shrewsbury.ac.uk>
2021-10-19 11:59:07 +0100

    Faibbanchi seqence (not yet working)

--- Computer science tasks 191021/Program.cs
+++ Computer science tasks 191021/Program.cs
@@ -6,7 +6,25 @@ namespace Computer_science_tasks_191021
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a, b, c, d;
+            bool e;
+            e = true;
+            a = 1;
+            b = 1;
+            c = 0;
+            Console.WriteLine("{0} , {1}", a, b);
+            while (e == true);
+                c = (a + b);
+                d = (c + b);
+                Console.WriteLine("{0} , {1}", c, d);
+                a = c;
+                b = d;
+            
+
+
+
+
+
         }
     }
 }
