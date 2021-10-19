11555c5af1cbafedbebb22265269365d3cb256a8
Drew Hunt <21234303@shrewsbury.ac.uk>
2021-10-19 09:54:08 +0100

    Add project files.

---
+++ Computer science tasks 191021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Computer_science_tasks_191021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
